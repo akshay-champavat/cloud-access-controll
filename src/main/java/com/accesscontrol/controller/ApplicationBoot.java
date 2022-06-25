4a9b29ce2ba5b822ed8aedb42b0be4fcf9d11fe7
Akshay Champavat <akshaychampavat@gmail.com>
2022-06-25 10:30:59 +0530

    package name change

--- src/main/java/com/accesscontrol/controller/ApplicationBoot.java
+++ src/main/java/com/accesscontrol/controller/ApplicationBoot.java
@@ -1,4 +1,4 @@
-package com.controller.boot;
+package com.accesscontrol.controller;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
